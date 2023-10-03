5b78ad084babab538c167177ade39f6323028736
salman <alysalman15@gmail.com>
2023-10-03 23:55:45 +0500

    code push

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-// var username = document.forms["myForm"]["name"].value;
-function requiredForm() {
-  var username = document.forms["myForm"]["name"].value;
-  var password = document.forms["myForm"]["password"].value;
-  if (username == "" || password == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+// var username = document.getElementById("username").value;
+// function requiredForm() {
+//   var username = document.forms["myForm"]["name"].value;
+//   var password = document.forms["myForm"]["password"].value;
+//   if (username == "" || password == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
 function showPost() {
+  var username = document.getElementById("username").value;
   var text = document.getElementById("textarea").value;
   var display = document.getElementById("screen");
   display.innerHTML += "<br />" + text;
   var newUser = document.getElementById("newUser");
   // newUser.innerHTML = "welcome " + username.value;
+  // document.writeln("your usernaem is : " + username);
   document.getElementById("textarea").value = "";
 }
