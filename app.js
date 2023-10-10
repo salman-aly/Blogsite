6db5c9991543b6ba9889172a08af77c172faee61
salman <alysalman15@gmail.com>
2023-10-10 18:16:42 +0500

    code push

--- app.js
+++ app.js
@@ -24,21 +24,30 @@
 function post() {
   var title = document.getElementById("title");
   var description = document.getElementById("descri");
+  if (title.value.trim() && description.value.trim()) {
+    var posts = document.getElementById("posts")
+    posts.innerHTML += `<div class="posts">
+    <div>
+      <h5>Post</h5>
+    </div>
+    <div>
+      <h3>${title.value}</h3>
+    </div>
+    <div>
+      <p>
+        ${description.value}
+      </p>
+    </div>
+  </div>`
 
-  var posts = document.getElementById("posts")
-  posts.innerHTML += `<div class="posts">
-  <div>
-    <h5>Post</h5>
-  </div>
-  <div>
-    <h3>${title.value}</h3>
-  </div>
-  <div>
-    <p>
-      ${description.value}
-    </p>
-  </div>
-</div>`
+    title.value = "";
+    description.value = "";
 
-
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Please enter title and description!',
+    })
+  }
 }
